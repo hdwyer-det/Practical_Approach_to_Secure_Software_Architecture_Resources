b4eb97485e78f21f5e1d4c78393d553d56ce68eb
Ben Jones <benpaddlejones@outlook.com>
2024-11-17 15:35:13 +1100

    session 1 source and template

--- Session_2_Flask_Intro/source/main.py
+++ Session_2_Flask_Intro/source/main.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
 
 app = Flask(__name__)
 
+is_logged_in = False
 
-@app.route("/", methods=["GET"])
-@app.route("/index.html", methods=["GET"])
-def index():
-    return render_template("/index.html")
+
+@app.route("/", methods=["POST", "GET"])
+def index_page():
+    global is_logged_in
+    if request.method == "POST":
+        username = request.form["username"]
+        app.logger.critical(f"Username is {username}")
+        password = request.form["password"]
+        app.logger.critical(f"Password is {password}")
+        is_logged_in = True
+        app.logger.critical(f"User is logged in ? {is_logged_in}")
+    return render_template("index.html", is_logged_in=is_logged_in)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
